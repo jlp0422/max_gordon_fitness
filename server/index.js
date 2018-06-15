b689f3da16d6516fe6a4cfd0935dbdd1bbaf0738
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-06-14 21:49:36 -0400

    react set up

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.use('/dist', express.static(path.join(__dirname, '../dist')))
+app.use('/vendor', express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '../public/index.html')))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`port of call: ${port}`))
