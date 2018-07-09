c9b99c1479be8d82d614ff6c64b31f49e101ca43
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-07-09 19:31:30 -0400

    started on nav - need to wireframe first

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
+/* eslint-disable */
 import React from 'react';
+import { Switch, HashRouter as Router, Route } from 'react-router-dom';
+import Nav from './Nav'
 
 class App extends React.Component {
   render() {
     return (
-      <hr />
+      <Router>
+        <Route path='/' component={ Nav } />
+      </Router>
     )
   }
 }
